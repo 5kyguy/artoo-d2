23c4f4e5503d382a6c68f545279e4ec87f4c38ea
5kyguy <0x5kyguy@gmail.com>
2026-03-01 22:15:05 +0530

    chore: migrate backgrounds

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -7,12 +7,11 @@ sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr
 # Single-theme setup: UI/themes/colors/fonts are applied once via config/ and default/.
 # No theme-set or subsequent scripted theme changes.
 
-# Set default background once (first image in default/backgrounds)
-if [[ -n ${OMARCHY_PATH:-} ]] && [[ -d "$OMARCHY_PATH/default/backgrounds" ]]; then
-  first_bg=$(find "$OMARCHY_PATH/default/backgrounds" -maxdepth 1 -type f \( -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' \) 2>/dev/null | head -1)
+# Set default background once (first image in backgrounds/)
+if [[ -n ${OMARCHY_PATH:-} ]] && [[ -d "$OMARCHY_PATH/backgrounds" ]]; then
+  first_bg=$(find "$OMARCHY_PATH/backgrounds" -maxdepth 1 -type f \( -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' \) 2>/dev/null | head -1)
   if [[ -n $first_bg ]]; then
-    mkdir -p "$OMARCHY_PATH/default"
-    ln -nsf "$first_bg" "$OMARCHY_PATH/default/background"
+    ln -nsf "$first_bg" "$OMARCHY_PATH/background"
   fi
 fi
 
