b05293c069c30e0f1be78486e84d53795ed15345
5kyguy <0x5kyguy@gmail.com>
2026-03-01 17:50:28 +0530

    feat: enable flatpaks in installation

---
+++ install/config/flatpak.sh
@@ -0,0 +1,3 @@
+# Add Flathub remote so Flatpak apps can be installed
+# (flatpak is in omarchy-base.packages; xdg-desktop-portal-* already configured)
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
